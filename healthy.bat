89cd391254bc009d0d6d4b1d79ba2168abee9b5e
Cal <caverym@outlook.com>
2019-05-19 21:59:59 -0400

    commands run

--- healthy.bat
+++ healthy.bat
@@ -1,7 +1,17 @@
 :: At a certain time, say a message concerned about users health
 @echo off
-set healthy=echo %TIME%
+cd %temp%
 echo %TIME%
+
 set slp=Go the fuck to sleep, bro. Its late and staying up late is unhealthy, bro.
 set wake=Sorry to interupt. Ignore me
-if %TIME%=="01:00:00" (cd c:\sleep.vbs) else (pause)
+set tm=%now%
+cls
+
+set "task=ifo"
+
+:ifo
+if "%tm%" lss "03:30:00,00" ( set "task=slp" ) else ( set "task=ift" )
+
+:ift
+if "%tm%" geq "22:00:00,00" ( set "task=wake" )
--- startup.bat
+++ startup.bat
@@ -1,7 +1,8 @@
 @echo off
-mkdir %temp%\healthy\
+
 xcopy /s /v /y "healthy.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-xcopy /s /v /y "sleep.vbs" "%temp%\healthy"
-xcopy /s /v /y "wake.vbs" "%temp%\healthy"
+xcopy /s /v /y "sleep.vbs" "%temp%"
+xcopy /s /v /y "wake.vbs" "%temp%"
 cls
-C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\healthy.bat
+cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
+healthy.bat
