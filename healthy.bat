a68855eeead3f915b8823bc600a9acf562075551
Cal <caverym@outlook.com>
2019-05-19 22:10:12 -0400

    lss 01

--- healthy.bat
+++ healthy.bat
@@ -2,22 +2,10 @@
 @echo off
 cd %temp%
 echo %TIME%
-
 set slp=Go the fuck to sleep, bro. Its late and staying up late is unhealthy, bro.
 set wake=Sorry to interupt. Ignore me
 set tm=%now%
 cls
-
 set "task=ifo"
-
 :ifo
-if "%tm%" lss "03:30:00,00" ( set "task=slp" ) else ( set "task=ift" )
-
-:ift
-if "%tm%" geq "22:00:00,00" ( set "task=wake" )
-
-:slp
-sleep.vbs
-
-:wake
-wake.vbs
+if "%tm%" lss "01:00:00,00" ( sleep.vbs ) else ( wake.vbs )
